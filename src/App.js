8cc994f201ccedf53b0dd4c59c9ead16c381fb96
Fletcher Johnson <samuel.f.johnson96@gmail.com>
2022-04-21 17:30:20 -1000

    typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 import Mainnav from './Components/Mainnav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Projectcard from './Components/Projectcard';
 import Bonsai from './Components/Bonsai';
-
+import Typewriter from './Components/Typewriter';
 const App = () => {
 
 
@@ -11,7 +12,27 @@ const App = () => {
   return (
     <div>
       <Bonsai/>
+      
       <Mainnav/>
+      <div className="img-txt-wrapper">
+        <h1
+          style={{ paddingTop: "5rem", margin: "auto 0", fontWeight: "normal" }}
+        >
+          Fletcher is {" "}
+          <span style={{ color: "black", fontWeight: "normal" }}>
+            <Typewriter
+              loop
+              cursor
+              typespeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+              words={[ "a Web Developer.", "an Entrepreneur.", "a Life Long Learner.", "persistant.", "fun!" ]}
+            />
+          </span>
+        </h1>
+      </div>
+      
+
       
       <Projectcard/>
     </div>
--- src/Components/Bonsai.jsx
+++ src/Components/Bonsai.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import BonsaiPic from '../Bonsai.JPG'
 import { Card } from 'react-bootstrap'
 const Bonsai = () => {
+
+    
+
   return (
     <div>
+      
+
       <Card.Img variant="top" src={BonsaiPic}/>
+      
+  
     </div>
   )
 }
